9b351a36c7ff671e3cd18cfd058b527cee286e77
Virginie-Villard <virginie.villard@le-campus-numerique.fr>
2021-08-27 11:57:42 +0200

    Etape 6

--- students/warrior.php
+++ students/warrior.php
@@ -3,9 +3,47 @@
 require_once __DIR__ . "/../base/localWarrior.php";
 
 
-$GLOBALS['warriorID'] = 'azertyuiop';
+// 1/ Modifiez la variable globale warriorID dans le fichier students/warrior.php
+$GLOBALS['warriorID'] = 'virginie';
 
 // Définissez vos class de combattants
 abstract class Warrior extends LocalWarrior {
+    // 4/ La classe Warrior doit avoir les attributs (publics) $id, $name, $speed, $life, $shield, $imageUrl et $weapon
+    public  $id;
+    public  $name;
+    public  $speed;
+    public  $life;
+    public  $shield;
+    public  $imageUrl;
+    public  $weapon;
 
+    
 };
+
+// 2/ Les classes StartrekWarrior, MarvelWarrior et PokemonWarrior doivent être créées dans le ficher students/warrior.php
+// extends
+// StartrekWarrior extends Warrior
+// glossaire : factoriser le code
+
+class StartrekWarrior extends Warrior {
+    //5/ Les classe StartrekWarrior, MarvelWarrior et PokemonWarrior doivent avoir respectivement les attributs $mentalPower, $superPower et $level
+    public $mentalPower;
+    public function power() {
+        return $this->mentalPower;
+    }
+}
+
+class MarvelWarrior extends Warrior {
+    public $superPower;
+    public function power() {
+        return $this->superPower;
+    }
+}
+class PokemonWarrior extends Warrior {
+    public $level;
+    public function power() {
+        return $this->level;
+        ;
+    }
+}
+
